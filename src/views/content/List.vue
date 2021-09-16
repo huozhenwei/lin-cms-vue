35f1941eaaaf43337df4566b6f7e48823f2c3356
huozhenwei <zhenwei.huo@bjrxtd.com>
2021-09-16 17:20:20 +0800

    期刊内容列表实现

--- src/views/content/List.vue
+++ src/views/content/List.vue
@@ -5,7 +5,37 @@
       <el-button type="primary">新增内容</el-button>
     </div>
     <div class="table-container">
-      <el-table :data="contentList"></el-table>
+      <el-table :data="contentList">
+        <el-table-column label="序号" width="60" type="index"></el-table-column>
+        <el-table-column label="类型" prop="type">
+          <template slot-scope="scope">
+            {{type[scope.row.type]}}
+          </template>
+        </el-table-column>
+        <el-table-column label="标题" prop="title"></el-table-column>
+        <el-table-column label="期刊介绍" prop="content"></el-table-column>
+        <el-table-column label="音乐外链" prop="url">
+          <template slot-scope="scope">
+            <audio controls v-if="scope.row.url">
+              <source :src="scope.row.url">
+            </audio>
+          </template>
+        </el-table-column>
+        <el-table-column label="发布时间" prop="pubdate" width="120"></el-table-column>
+        <el-table-column label="点赞数" prop="fav_nums" width="120"></el-table-column>
+        <el-table-column label="是否有效" prop="status" width="120">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 1" type="success">有效</el-tag>
+            <el-tag v-else type="info">无效</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" fixed="right">
+          <template slot-scope="scope">
+            <el-button @click="handleEdit(scope.row)" v-permission="{ permission: '编辑内容', type: 'disabled'}">编辑</el-button>
+            <el-button type="danger" @click="handleDelete(scope.row)" v-permission="{ permission: '删除内容', type: 'disabled'}">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
@@ -18,6 +48,11 @@ export default {
   data() {
     return {
       contentList: [],
+      type: {
+        100: '电影',
+        200: '音乐',
+        300: '句子'
+      }
     }
   },
   created() {
@@ -27,6 +62,12 @@ export default {
     async getContentList() {
       this.contentList = await ContentModel.getContentList()
     },
+    handleEdit(row) {
+      console.log(row)
+    },
+    handleDelete(row) {
+      console.log(row)
+    }
   },
 }
 </script>
